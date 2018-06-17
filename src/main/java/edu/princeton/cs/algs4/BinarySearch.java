0526a8a38b97159cc3c15503ad3a9936eba0f316
陈宏伟 <chenhongwei1112@163.com>
2018-06-18 00:18:30 +0800

    1.优化格式.

--- src/main/java/edu/princeton/cs/algs4/BinarySearch.java
+++ src/main/java/edu/princeton/cs/algs4/BinarySearch.java
@@ -57,11 +57,14 @@ public class BinarySearch {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
-            // Key is in a[lo..hi] or not present.
-            int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
-            else return mid;
+            int mid = lo + (hi - lo) / 2; 
+            if (key < a[mid]) {
+                hi = mid - 1;
+            }else if (key > a[mid]) {
+                lo = mid + 1;
+            }else {
+                return mid;
+            }
         }
         return -1;
     }
